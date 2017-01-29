be093883ad824d6f9b8d62a7cc2d2b00e122bcf4
Gee Li <geeyung.li@gmail.com>
2017-01-29 06:18:26 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |name,index|
+    puts "#{index+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |shout|
+    shout.capitalize<<"!" #you can push a string to each individual element with <<
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |call|
+    call.length>4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)# code an argument here
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |food| #find looks for first match that meets condition and returns value
+      cheese_types.include?(food) #include? looks through array for any matches.
+    end
 end
